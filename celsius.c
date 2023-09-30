d587ae8ad2d60fba413be000c0e7f39aa644dfa2
Isaac Turner <spacey_sooty@outlook.com>
2023-09-30 22:52:28 +0800

    add celsius.c

---
+++ celsius.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * fahr - 32 / 9;
+		printf("Farenheight: %d\n", fahr);
+		printf("Celsius: %d\n", celsius);
+		fahr += 20;
+	}
+	return 0;
+}
+
